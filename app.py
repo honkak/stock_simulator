da95ff495667d364b2583d4348ba043524747fe9
honkak <149265734+honkak@users.noreply.github.com>
2025-10-20 23:53:00 +0900

    Update app.py

--- app.py
+++ app.py
@@ -345,7 +345,7 @@ if codes:
         fig.update_layout(
             updatemenus=[dict(type="buttons",
                               x=1.03,  # ⭐ 왼쪽으로 이동 (1.21 -> 1.05)
-                              y=0.3,  # ⭐ 아래로 이동 (0.7 -> 0.25)
+                              y=0.5,  # ⭐ 아래로 이동 (0.7 -> 0.25)
                               showactive=False,
                               xanchor='left', # x=1.05를 기준으로 버튼을 왼쪽에 고정
                               yanchor='middle', # y=0.25를 기준으로 버튼을 중앙에 고정
@@ -371,6 +371,231 @@ if codes:
     # ----------------------------------------------------------
     # 6. 최종 요약 테이블 표시
     # ----------------------------------------------------------
-    # 🎯 [수정] 항상 최종 요약표를 표시
     # monthly_amount_krw를 추가 인수로 전달
     display_final_summary_table(data, cumulative_principal, monthly_amount_krw)
+
+# ==============================================================================
+# 4. 종목 코드 참고 자료 섹션 (추가됨)
+# ==============================================================================
+
+# 수평선 추가
+st.markdown("---")
+
+# 체크박스 그룹
+col1, col2, col3, col4 = st.columns(4)
+with col1:
+    show_us_etf = st.checkbox("미국ETF", value=False)  # 미국ETF
+with col2:
+    show_kr_etf = st.checkbox("한국ETF", value=False)  # 한국ETF
+with col3:
+    show_major_stocks = st.checkbox("주요종목", value=False)  # 주요종목
+with col4:
+    show_major_index = st.checkbox("지수", value=False)  # 지수
+
+
+# '미국ETF' 체크박스와 연결된 데이터 행렬
+data_matrix_us_etf = [
+    ['-3X', '-2X', '-1X', '코드', '1X', '2X', '3X'],  # 1행
+    ['SPXU', 'SDS', 'SH', 'S&P500', 'SPY', 'SSO', 'UPRO'],  # 2행
+    ['SQQQ', 'QID', 'PSQ', '나스닥100', 'QQQ', 'QLD', 'TQQQ'],  # 3행
+    ['SDOW', 'DXD', 'DOG', '다우존스', 'DIA', 'DDM', 'UDOW'],  # 4행
+    ['TZA', 'TWM', 'RWM', '러셀2000', 'IWM', 'UWM', 'TNA'],  # 5행
+    ['', '', '', '한국', 'EWY', 'KORU', ''],  # 6행
+    ['YANG', 'FXP', 'CHAD', '중국', 'FXI', 'CHAU', 'YINN'],  # 7행
+    ['', 'EWV', '', '일본', 'EWJ', 'EZJ', 'JPNL'],  # 8행
+    ['', '', '', '베트남', 'VNM', '', ''],  # 9행
+    ['INDZ', '', '', '인도', 'INDA', '', 'INDL'],  # 10행
+    ['RUSS', '', '', '러시아', 'RSX', '', 'RUSL'],  # 11행
+    ['', 'BZQ', '', '브라질', 'EWZ', '', 'BRZU'],  # 12행
+    ['DGLD', 'GLL', 'DGZ', '금', 'GLD', 'DGP', 'UGLD'],  # 13행
+    ['DSLV', 'ZSL', '', '은', 'SLV', 'AGQ', 'USLV'],  # 14행
+    ['DWT', 'SCO', '', '원유', 'USO', 'UCO', ''],  # 15행
+    ['DGAZ', 'KOLD', '', '천연가스', 'UNG', 'BOIL', 'UGAZ'],  # 16행
+    ['', '', '', '농산물', 'DBA', '', ''],  # 17행
+]
+
+# '지수' 체크박스와 연결된 데이터 행렬
+data_matrix_index = [
+    ['한국코드', '설명', '미국코드', '설명', '기타코드', '설명'],  # 1행
+    ['KS11', 'KOSPI지수', 'DJI', '다우존스', 'JP225', '닛케이225선물'],  # 2행
+    ['KQ11', 'KOSDAQ지수', 'IXIC', '나스닥', 'STOXX50E', 'EuroStoxx50'],  # 3행
+    ['KS50', 'KOSPI50지수', 'GSPC', 'S&P500', 'CSI300', 'CSI300(중국)'],  # 4행
+    ['KS100', 'KOSPI100', 'VIX', 'S&P500VIX', 'HSI', '항셍(홍콩)'],  # 5행
+    ['KRX100', 'KRX100', '-', '-', 'FTSE', '영국FTSE'],  # 6행
+    ['KS200', '코스피200', '-', '-', 'DAX', '독일DAX30'],  # 7행
+]
+
+# 시가총액 상위 11개 종목 데이터 행렬
+data_matrix_top_stocks = [
+    ['미국종목코드', '설명', '한국종목코드', '설명'],  # 1행
+    ['AAPL', '애플', '005930', '삼성전자'],  # 2행
+    ['MSFT', '마이크로소프트', '000660', 'SK하이닉스'],  # 3행
+    ['AMZN', '아마존', '373220', 'LG에너지솔루션'],  # 4행
+    ['NVDA', '엔비디아', '207940', '삼성바이오로직스'],  # 5행
+    ['GOOGL', '알파벳A', '005380', '현대차'],  # 6행
+    ['META', '메타', '068270', '셀트리온'],  # 7행
+    ['TSLA', '테슬라', '000270', '기아'],  # 8행
+    ['BRK.B', '버크셔헤서웨이', '196170', '알테오젠'],  # 9행
+    ['UNH', '유나이티드헬스', '247540', '에코프로비엠'],  # 10행
+    ['JNJ', '존슨앤존슨', '086520', '에코프로'],  # 11행
+]
+
+# 한국ETF 체크박스와 연결된 데이터 행렬 (4열)
+data_matrix_kr_etf = [
+    ['한국종목코드', '설명', '한국종목코드', '설명'],  # 열 제목
+    ['069500', 'KODEX 200', '122630', 'KODEX 레버리지'],
+    ['229200', 'KODEX 코스닥150', '233740', 'KODEX 코스닥150레버리지'],
+    ['114800', 'KODEX 인버스', '252670', 'KODEX 200선물인버스2X'],
+    ['251340', 'KODEX 코스닥150선물인버스', '442580', 'PLUS 글로벌HBM반도체'],
+    ['243890', 'TIGER 200에너지화학레버리지', '412570', 'TIGER 2차전지TOP10레버리지'],
+    ['463640', 'KODEX 미국S&P500유틸리티', '379800', 'KODEX 미국S&P500TR'],
+    ['379810', 'KODEX 미국나스닥100TR', '449190', 'KODEX 미국나스닥100(H)'],
+    ['409820', 'KODEX 미국나스닥100레버리지(합성 H)', '438100', 'ACE 미국나스닥100채권혼합액티브'],
+    ['447660', 'PLUS 애플채권혼합', '448540', 'ACE 엔비디아채권혼합블룸버그'],
+    ['236350', 'TIGER 인도니프티50레버리지(합성)', '132030', 'KODEX 골드선물(H)'],
+    ['144600', 'KODEX 은선물(H)', '530063', '삼성 레버리지 구리 선물 ETN(H)'],
+    ['530031', '삼성 레버리지 WTI원유 선물 ETN', '530036', '삼성 인버스 2X WTI원유 선물 ETN'],
+    ['438320', 'TIGER 차이나항셍테크레버리지(합성 H)', '371460', 'TIGER 차이나전기차SOLACTIVE'],
+]
+
+# '주요종목' 체크박스를 선택할 때 표 출력
+if show_major_index or show_major_stocks or show_us_etf or show_kr_etf:
+    # 미국ETF 표 출력
+    if show_us_etf:
+        st.markdown("<h4 style='font-size: 16px; text-align: left; margin-top: 20px;'>미국 ETF 주요 코드</h4>", unsafe_allow_html=True)
+        html = '''
+        <style>
+        table {
+            border-collapse: collapse;  
+            width: 100%;  
+            font-size: 10px;  /* 글자 크기를 10px로 설정 */
+        }
+        td {
+            border: 1px solid black;  
+            padding: 8px;  
+            text-align: center;
+            /* color: #333333; <-- 일반 셀의 텍스트 색상 고정 제거 */
+        }
+        .highlight {
+            background-color: lightgray;
+            color: #333333; /* 다크 모드에서도 대비가 확보되도록 텍스트 색상을 어두운 회색으로 고정 */
+        }
+        </style>
+        <table>
+        '''
+
+        for i, row in enumerate(data_matrix_us_etf):
+            html += '<tr>'
+            for j, cell in enumerate(row):
+                if i == 0 or j == 3:  # 첫 번째 행과 코드 열 강조
+                    html += f'<td class="highlight">{cell}</td>'
+                else:
+                    html += f'<td>{cell}</td>'
+            html += '</tr>'
+        html += '</table>'
+        st.markdown(html, unsafe_allow_html=True)
+
+    # 한국ETF 표 출력
+    if show_kr_etf:
+        st.markdown("<h4 style='font-size: 16px; text-align: left; margin-top: 20px;'>한국 ETF 주요 코드</h4>", unsafe_allow_html=True)
+        html_kr_etf = '''
+        <style>
+        table {
+            border-collapse: collapse;  
+            width: 100%;  
+            font-size: 10px;  /* 글자 크기를 10px로 설정 */
+        }
+        td {
+            border: 1px solid black;  
+            padding: 8px;  
+            text-align: center;
+            /* color: #333333; <-- 일반 셀의 텍스트 색상 고정 제거 */
+        }
+        .highlight {
+            background-color: lightgray;
+            color: #333333; /* 다크 모드에서도 대비가 확보되도록 텍스트 색상을 어두운 회색으로 고정 */
+        }
+        </style>
+        <table>
+        '''
+
+        for i, row in enumerate(data_matrix_kr_etf):
+            html_kr_etf += '<tr>'
+            for j, cell in enumerate(row):
+                if i == 0 or j == 1 or j == 3:  # 첫 번째 행과 코드 열 강조
+                    html_kr_etf += f'<td class="highlight">{cell}</td>'
+                else:
+                    html_kr_etf += f'<td>{cell}</td>'
+            html_kr_etf += '</tr>'
+        html_kr_etf += '</table>'
+        st.markdown(html_kr_etf, unsafe_allow_html=True)
+
+    # 주요종목 표 출력
+    if show_major_stocks:
+        st.markdown("<h4 style='font-size: 16px; text-align: left; margin-top: 20px;'>주요 시가총액 상위 종목 코드</h4>", unsafe_allow_html=True)
+        html_major_stocks = '''
+        <style>
+        table {
+            border-collapse: collapse;  
+            width: 100%;  
+            font-size: 10px;  /* 글자 크기를 10px로 설정 */
+        }
+        td {
+            border: 1px solid black;  
+            padding: 8px;  
+            text-align: center;
+            /* color: #333333; <-- 일반 셀의 텍스트 색상 고정 제거 */
+        }
+        .highlight {
+            background-color: lightgray;
+            color: #333333; /* 다크 모드에서도 대비가 확보되도록 텍스트 색상을 어두운 회색으로 고정 */
+        }
+        </style>
+        <table>
+        '''
+
+        for i, row in enumerate(data_matrix_top_stocks):
+            html_major_stocks += '<tr>'
+            for j, cell in enumerate(row):
+                if i == 0 or j == 1 or j == 3:  # 첫 번째 행과 코드 열 강조
+                    html_major_stocks += f'<td class="highlight">{cell}</td>'
+                else:
+                    html_major_stocks += f'<td>{cell}</td>'
+            html_major_stocks += '</tr>'
+        html_major_stocks += '</table>'
+        st.markdown(html_major_stocks, unsafe_allow_html=True)
+
+    # 지수 표 출력
+    if show_major_index:
+        st.markdown("<h4 style='font-size: 16px; text-align: left; margin-top: 20px;'>주요 지수 코드</h4>", unsafe_allow_html=True)
+        html_index = '''
+        <style>
+        table {
+            border-collapse: collapse;  
+            width: 100%;  
+            font-size: 10px;  /* 글자 크기를 10px로 설정 */
+        }
+        td {
+            border: 1px solid black;  
+            padding: 8px;  
+            text-align: center;
+            /* color: #333333; <-- 일반 셀의 텍스트 색상 고정 제거 */
+        }
+        .highlight {
+            background-color: lightgray;
+            color: #333333; /* 다크 모드에서도 대비가 확보되도록 텍스트 색상을 어두운 회색으로 고정 */
+        }
+        </style>
+        <table>
+        '''
+
+        for i, row in enumerate(data_matrix_index):
+            html_index += '<tr>'
+            for j, cell in enumerate(row):
+                if i == 0 or j == 1 or j == 3 or j == 5:  # 첫 번째 행과 코드 열 강조
+                    html_index += f'<td class="highlight">{cell}</td>'
+                else:
+                    html_index += f'<td>{cell}</td>'
+            html_index += '</tr>'
+        html_index += '</table>'
+        st.markdown(html_index, unsafe_allow_html=True)
